deae7a5bae9af1f209cefac950ff92ea9374ab8a
LilitArz <lilitarzzz@gmail.com>
2018-03-18 23:47:59 +0400

    reducer: add reducer in index.js file

---
+++ src/reducer/index.js
@@ -0,0 +1,15 @@
+const initialState = {
+  link: ""
+}
+
+export const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case "ADD_LINK":
+      return {
+        ...state,
+        link: action.value
+      }
+    default:
+      return state
+  }
+}
