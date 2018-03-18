03be6f25e74ab4be1d987a5b5fe6c1e92832291d
LilitArz <lilitarzzz@gmail.com>
2018-03-19 00:38:49 +0400

    src: add NavBar in VideoHolder component to render

--- src/components/VideoHolder.js
+++ src/components/VideoHolder.js
@@ -4,9 +4,16 @@ import { store } from "../redux/store"
 import { connect } from "react-redux"
 import { bindActionCreators } from "redux"
 
-import { playVideo, addLink, pauseVideo, changeDuration } from "../redux/action"
+import {
+  playVideo,
+  addLink,
+  pauseVideo,
+  changeDuration,
+  changeVolume
+} from "../redux/action"
 import { Button } from "./Button"
 import { Video } from "./Video"
+import { Navbar } from "./Navbar"
 
 const actionCreators = dispatch =>
   bindActionCreators(
@@ -14,7 +21,8 @@ const actionCreators = dispatch =>
       playVideo,
       addLink,
       pauseVideo,
-      changeDuration
+      changeDuration,
+      changeVolume
     },
     dispatch
   )
@@ -22,7 +30,8 @@ const mapStateToProps = store => {
   return {
     videoDuration: store.videoDuration,
     link: store.link,
-    isPaused: store.isPaused
+    isPaused: store.isPaused,
+    projectVolume: store.projectVolume
   }
 }
 
@@ -42,6 +51,15 @@ class VideoHolder extends Component {
           playVideo={this.props.playVideo}
           pauseVideo={this.props.pauseVideo}
           changeVideoDuration={this.props.changeDuration}
+        />
+        <Navbar
+          videoLink={this.props.link}
+          changeVolume={this.props.changeVolume}
+          projectVolume={this.props.projectVolume}
+          videoDuration={this.props.videoDuration}
+          isPaused={this.props.isPaused}
+          playVideo={this.props.playVideo}
+          pauseVideo={this.props.pauseVideo}
         />
         <Button addLink={this.props.addLink} />
       </div>
