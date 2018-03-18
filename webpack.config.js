7e5aa7a195e04259a3e22568d212a91426b1af60
LilitArz <lilitarzzz@gmail.com>
2018-03-18 23:00:26 +0400

    add webpack configuration

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin")
+
+module.exports = {
+  entry: "./src/root",
+  output: {
+    path: __dirname + "/dist",
+    filename: "source.js"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"]
+          }
+        }
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/root/index.html",
+      filename: "index.html",
+      inject: "body"
+    })
+  ]
+}
