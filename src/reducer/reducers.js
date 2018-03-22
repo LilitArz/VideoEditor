55556d1cdee1b8dcdfa42f799e9701fb2b74fbe8
LilitArz <lilitarzzz@gmail.com>
2018-03-23 00:21:54 +0400

    delete reducer and sliceReducer files

--- src/reducer/reducers.js
+++
@@ -1,8 +0,0 @@
-import { reducer } from "../reducer/index"
-import { sliceReducer } from "../reducer/sliceReducer"
-import { combineReducers } from "redux"
-
-export default combineReducers({
-  reducer,
-  sliceReducer
-})
