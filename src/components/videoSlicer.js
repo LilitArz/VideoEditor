cbf896619603a9778884f4e88a512e2fd6720d13
LilitArz <lilitarzzz@gmail.com>
2018-03-23 00:11:44 +0400

    src: add div for controlling sliced videos in SlicedVideo file

--- src/components/videoSlicer.js
+++ src/components/videoSlicer.js
@@ -1,35 +1,107 @@
 import React, { Component } from "react"
+import { Slider } from "./Slider"
+import { Video } from "./Video"
 
 export class VideoSlicer extends Component {
   render() {
     return (
       <div
         style={{
-          float: "left",
-          width: "70%"
+          width: "70%",
+          float: "left"
         }}
       >
         {this.props.slicedArray.map((item, key) => {
           return (
-            <div
-              style={{
-                width: item.width,
-                height: "30px",
-                borderRadius: "25px",
-                backgroundColor: "green",
-                position: "relative",
-                top: "100px",
-                float: "left"
-              }}
-              onClick={event => {
-                const percent = Math.round(
-                  (event.clientX - event.target.offsetLeft) /
-                    event.target.offsetWidth *
-                    100
-                )
-                this.props.setParameters(percent, key)
-              }}
-            />
+            <div>
+              <div
+                style={{
+                  width: item.width,
+                  height: "30px",
+                  borderRadius: "25px",
+                  backgroundColor: "green",
+                  float: "left",
+                  position: "relative"
+                }}
+                onClick={event => {
+                  const percent = Math.round(
+                    (event.clientX - event.target.offsetLeft) /
+                      event.target.offsetWidth *
+                      100
+                  )
+                  this.props.setParameters(percent, key)
+
+                  this.props.clicked(key) //showslicer
+                }}
+              >
+                <Slider />
+              </div>
+              <ul
+                style={{
+                  listStyleType: "none",
+                  visibility: this.props.slicedDurationArray[key].isHovered
+                    ? "visible"
+                    : "hidden",
+                  width: "200px",
+                  borderRadius: "25px",
+                  position: "relative",
+                  top: "140px",
+                  left: "100px",
+                  height: "30px",
+                  backgroundColor: " #C0C0C0",
+                  float: "left"
+                }}
+              >
+                <li>
+                  <img
+                    src="/photos/playButton.png"
+                    style={{
+                      height: "20px",
+                      width: "20px",
+                      position: "relative",
+                      padding: "5px 5px"
+                    }}
+                    onClick={() => this.props.playSlicedVideo(key)}
+                  />
+                </li>
+                <li>
+                  <img
+                    src="/photos/Next.png"
+                    style={{
+                      height: "20px",
+                      width: "20px",
+                      position: "relative",
+                      bottom: "30px",
+                      left: "100px"
+                    }}
+                  />
+                </li>
+                <li>
+                  <img
+                    src="/photos/Previous.png"
+                    style={{
+                      height: "20px",
+                      width: "20px",
+                      position: "relative",
+                      bottom: "53px",
+                      left: "60px"
+                    }}
+                  />
+                </li>
+                <li>
+                  <img
+                    src="/photos/cross.png"
+                    style={{
+                      height: "20px",
+                      width: "20px",
+                      position: "relative",
+                      bottom: "77px",
+                      left: "140px"
+                    }}
+                  />
+                </li>
+              </ul>
+            </div>
           )
         })}
         <img
@@ -38,11 +110,11 @@ export class VideoSlicer extends Component {
             width: "25px",
             height: "25px",
             position: "relative",
-            top: "73px",
-            left: "1010px",
+            bottom: "27px",
+            left: "770px",
             float: "left"
           }}
-          onClick={event => {
+          onClick={() => {
             this.props.slicer()
           }}
         />
