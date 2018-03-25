92bf82267dcdab48854975ba20ec1768fa2c1646
LilitArz <lilitarzzz@gmail.com>
2018-03-25 21:12:08 +0400

    config: add rules for css

--- webpack.config.js
+++ webpack.config.js
@@ -9,6 +9,10 @@ module.exports = {
   module: {
     rules: [
       {
+        test: /\.css$/,
+        use: [{ loader: "style-loader" }, { loader: "css-loader" }]
+      },
+      {
         test: /\.js$/,
         exclude: /(node_modules)/,
         use: {
