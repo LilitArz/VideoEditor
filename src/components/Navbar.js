0df00b166a79e96a56c958f4b7014a36120b99e9
LilitArz <lilitarzzz@gmail.com>
2018-03-27 15:21:42 +0400

    src: separate styles from elements in navBar.js file

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,32 +1,80 @@
 import React, { Component } from "react"
-
+const styles = {
+  ul: {
+    listStyleType: "none",
+    width: "70%",
+    height: "7%",
+    backgroundColor: "rgb(199, 186, 186)",
+    float: "left"
+  },
+  li: {
+    float: "left"
+  },
+  playButton: {
+    width: "40px",
+    height: "40px",
+    display: "block",
+    position: "relative",
+    left: "80px"
+  },
+  fullDuration: {
+    padding: "50px 15px 80px 15px",
+    position: "relative",
+    top: "20px",
+    left: "150px"
+  },
+  duration: {
+    padding: "20px 15px 80px 15px",
+    fontSize: "25px",
+    display: "block",
+    textAlign: "center",
+    position: "relative",
+    left: "150px"
+  },
+  projectVolume: {
+    padding: "50px 15px 80px ",
+    position: "relative",
+    top: "15px",
+    right: "150px"
+  },
+  undo: {
+    width: "40px",
+    height: "40px",
+    display: "block",
+    position: "relative",
+    left: "250px"
+  },
+  redo: {
+    width: "40px",
+    height: "40px",
+    display: "block",
+    position: "relative",
+    left: "330px"
+  },
+  range: {
+    padding: "20px 40px",
+    position: "relative",
+    width: "30px",
+    bottom: "15px",
+    display: "block"
+  },
+  righFloat: {
+    float: "right"
+  }
+}
 export class Navbar extends Component {
   render() {
     return (
       <ul
         style={{
-          listStyleType: "none",
-          width: "70%",
-          height: "7%",
-          backgroundColor: "rgb(199, 186, 186)",
-          float: "left",
+          ...styles.ul,
           visibility: this.props.videoLink == "" ? "hidden" : "visible"
         }}
       >
-        <li
-          style={{
-            float: "left"
-          }}
-        >
+        <li style={styles.li}>
           <img
             src="/photos/playButton.png"
-            style={{
-              width: "40px",
-              height: "40px",
-              display: "block",
-              position: "relative",
-              left: "80px"
-            }}
+            style={styles.playButton}
             onClick={e => {
               this.props.isPaused
                 ? this.props.playVideo()
@@ -34,105 +82,26 @@ export class Navbar extends Component {
             }}
           />
         </li>
-        <li
-          style={{
-            float: "left"
-          }}
-        >
-          <span
-            style={{
-              padding: "50px 15px 80px 15px",
-              position: "relative",
-              top: "20px",
-              left: "150px"
-            }}
-          >
-            Full Duration
-          </span>
-          <span
-            style={{
-              padding: "20px 15px 80px 15px",
-              fontSize: "25px",
-              display: "block",
-              textAlign: "center",
-              position: "relative",
-              left: "150px"
-            }}
-          >
-            {this.props.videoDuration}
-          </span>
+        <li style={styles.li}>
+          <span style={styles.fullDuration}>Full Duration</span>
+          <span style={styles.duration}>{this.props.videoDuration}</span>
         </li>
-        <li
-          style={{
-            float: "left"
-          }}
-        >
-          <img
-            src="/photos/myundo.png"
-            style={{
-              width: "40px",
-              height: "40px",
-              display: "block",
-              position: "relative",
-              left: "250px"
-            }}
-          />
+        <li style={styles.li}>
+          <img src="/photos/myundo.png" style={styles.undo} />
         </li>
-        <li
-          style={{
-            float: "left"
-          }}
-        >
-          <img
-            src="/photos/myredo.png"
-            style={{
-              width: "40px",
-              height: "40px",
-              display: "block",
-              position: "relative",
-              left: "330px"
-            }}
-          />
+        <li style={styles.li}>
+          <img src="/photos/myredo.png" style={styles.redo} />
         </li>
-        <li
-          style={{
-            float: "right"
-          }}
-        >
-          <span
-            style={{
-              padding: "50px 15px 80px ",
-              position: "relative",
-              top: "15px",
-              right: "150px"
-            }}
-          >
-            Project Volume
-          </span>
-          <span
-            style={{
-              padding: "50px 15px 80px 15px",
-              position: "relative",
-              top: "40px",
-              right: "230px",
-              fontSize: "25px"
-            }}
-          >
-            {this.props.projectVolume}
-          </span>
+        <li style={styles.righFloat}>
+          <span style={styles.projectVolume}>Project Volume</span>
+          <span style={styles.duration}>{this.props.projectVolume}</span>
           <input
             type="range"
             value={this.props.projectVolume}
             onChange={e => {
               this.props.changeVolume(e.target.value)
             }}
-            style={{
-              padding: "20px 40px",
-              position: "relative",
-              width: "30px",
-              bottom: "15px",
-              display: "block"
-            }}
+            style={styles.range}
           />
         </li>
       </ul>
