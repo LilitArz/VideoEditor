66040a7056af0367f30ff19d1b701a0c50d84ac8
LilitArz <lilitarzzz@gmail.com>
2018-04-04 13:17:11 +0400

    src: add test component for Testing to work with pug file

---
+++ src/components/Test.js
@@ -0,0 +1,76 @@
+import React, { Component } from "react"
+const pugFile = require("../../pug/index.pug")
+const zibil = pugFile.replace("import React from 'react;", "")
+import convert from "htmr"
+console.log(pugFile)
+
+export class Test extends Component {
+  constructor() {
+    super()
+    this.myDiv = 0
+    this.input = 0
+    this.image = 0
+    this.container = 0
+  }
+  delay = e => {
+    e.stopPropagation()
+  }
+  func = e => {
+    e.stopPropagation()
+    this.myDiv = document.getElementById(e.target.id)
+
+    switch (e.target.nodeName) {
+      case "IMG":
+        this.input = document.createElement("input")
+        this.container.appendChild(this.input)
+        this.input.autofocus = true
+        this.input.value = e.target.src
+        break
+      case "P":
+      case "DIV":
+        const text = this.myDiv.innerHTML
+        this.myDiv.innerHTML = "<input id ='input' autofocus />"
+        this.input = document.getElementById("input")
+        this.input.value = text
+      default:
+        break
+    }
+    this.input.addEventListener("click", this.delay)
+    this.myDiv.removeEventListener("click", this.func)
+  }
+
+  changeDivValue = () => {
+    if (this.input !== 0) {
+      switch (this.myDiv.nodeName) {
+        case "IMG":
+          this.myDiv.src = this.input.value
+          this.input.parentElement.removeChild(this.input)
+          break
+        case "P":
+        case "DIV":
+          this.myDiv.innerHTML = this.input.value
+          break
+        default:
+          break
+      }
+      this.myDiv.addEventListener("click", this.func)
+      this.input = 0
+    }
+  }
+
+  componentDidMount() {
+    this.container = document.getElementsByClassName("container")[0]
+    console.log(this.container)
+    for (let i = 0; i < this.container.childNodes.length; ++i) {
+      this.container.childNodes[i].addEventListener("click", this.func)
+    }
+    document.body.addEventListener("click", this.changeDivValue)
+  }
+
+  render() {
+    function createMarkup() {
+      return { __html: pugFile }
+    }
+    return <div dangerouslySetInnerHTML={createMarkup()} />
+  }
+}
